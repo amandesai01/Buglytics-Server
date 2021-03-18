b29bcc42fc323f99f25291f41765a8051a13d32a
Aman Desai <amandesai01@gmail.com>
2021-03-18 22:13:58 +0530

    interfaces: projects

---
+++ BuglyticsServer/Interfaces/projects.py
@@ -0,0 +1,37 @@
+from uuid import uuid4
+from time import time
+
+from BuglyticsServer.Datastore import projects as proj_db
+
+def create_project(title, details, organization_id):
+    project = {
+        "project_id": str(uuid4()),
+        "title": title,
+        "details": details,
+        "created_ts": str(int(time()))
+    }
+    proj_db.insert_project(project, organization_id)
+    return project
+
+def get_all_projects(organization_id):
+    data_raw = proj_db.select_all_projects(organization_id)
+    data = []
+    for proj_raw in data_raw:
+        data.append({
+            "project_id": proj_raw[0],
+            "title": proj_raw[1],
+            "details": proj_raw[2],
+            "created_ts": proj_raw[3],
+            "bug_count": proj_raw[4]
+        })
+
+def get_selected_project(project_id, organization_id):
+    proj_raw = proj_db.select_specific_project(project_id, organization_id)
+    project = {
+            "project_id": proj_raw[0],
+            "title": proj_raw[1],
+            "details": proj_raw[2],
+            "created_ts": proj_raw[3],
+            "bug_count": proj_raw[4]
+        }
+    return project
