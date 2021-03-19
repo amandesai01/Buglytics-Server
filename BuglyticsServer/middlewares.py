f6941bd30c5a9337a188e5518b9f40a76b71a5dc
Aman Desai <amandesai01@gmail.com>
2021-03-19 12:48:50 +0530

    middleware fixes - trycatch protected

--- BuglyticsServer/middlewares.py
+++ BuglyticsServer/middlewares.py
@@ -12,17 +12,23 @@ def get_decoded_token_from_header(request_header):
 def error_report_access_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        obj = get_decoded_token_from_header(request.headers)
-        if "ERR_REPORT" in obj['ACCESS_RIGHTS']:
-            return f(obj['PROJ_ID'], obj['ORG_ID'])
-        return jsonify({ "STATUS" : "FAIL", "MSG": "Contact Organization Owner to Provide Access." }), 401
+        try:
+            obj = get_decoded_token_from_header(request.headers)
+            if "ERR_REPORT" in obj['ACCESS_RIGHTS']:
+                return f(obj['PROJ_ID'], obj['ORG_ID'])
+            return jsonify({ "STATUS" : "FAIL", "MSG": "Contact Organization Owner to Provide Access." }), 401
+        except:
+            return jsonify({"STATUS": "FAIL", "MSG": "ERROR IN AUTHENTICATION"}), 401
     return decorated_function
 
 def error_view_access_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        obj = get_decoded_token_from_header(request.headers)
-        if "ERR_VIEW" in obj['ACCESS_RIGHTS']:
-            return f(obj['PROJ_ID'], obj['ORG_ID'])
-        return jsonify({ "STATUS" : "FAIL", "MSG": "Contact Organization Owner to Provide Access." }), 401
+        try:
+            obj = get_decoded_token_from_header(request.headers)
+            if "ERR_VIEW" in obj['ACCESS_RIGHTS']:
+                return f(obj['PROJ_ID'], obj['ORG_ID'])
+            return jsonify({ "STATUS" : "FAIL", "MSG": "Contact Organization Owner to Provide Access." }), 401
+        except:
+            return jsonify({"STATUS": "FAIL", "MSG": "ERROR IN AUTHENTICATION"}), 401
     return decorated_function
