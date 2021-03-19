bca4f08604130529d0655c3907f183299cf486b1
Aman Desai <amandesai01@gmail.com>
2021-03-19 13:19:29 +0530

    organizations have emails

--- BuglyticsServer/Interfaces/organizations.py
+++ BuglyticsServer/Interfaces/organizations.py
@@ -2,20 +2,27 @@ from uuid import uuid4
 from time import time
 from hashlib import sha256
 
+from psycopg2 import errors as pgerrors
+
 from BuglyticsServer.Datastore import organizations as org_db
 from BuglyticsServer.validations import validate_secret
+from BuglyticsServer.exceptions import EmailAlreadyExistsException
 
-def create_organization(title, secret):
-    validate_secret(secret)
-    organization = {
-        "organization_id": str(uuid4()),
-        "title": title,
-        "created_ts": str(int(time())),
-        "secret": sha256(secret.encode('utf-8')).hexdigest()
-    }
-    org_db.insert_organization(organization)
-    organization['total_projects'] = 0
-    return organization
+def create_organization(title, email, secret):
+    try:
+        validate_secret(secret)
+        organization = {
+            "organization_id": str(uuid4()),
+            "title": title,
+            "created_ts": str(int(time())),
+            "secret": sha256(secret.encode('utf-8')).hexdigest(),
+            "email": email
+        }
+        org_db.insert_organization(organization)
+        organization['total_projects'] = 0
+        return organization
+    except pgerrors.UniqueViolation:
+        raise EmailAlreadyExistsException
 
 def get_organization_details(organization_id):
     data = org_db.select_organization(organization_id)
