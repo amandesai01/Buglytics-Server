cca6fdbb7fd31af215a911a921cd5745fff5836d
Aman Desai <amandesai01@gmail.com>
2021-03-19 20:58:51 +0530

    migrate: error reporting service only needs to catch errors

--- server/services/error_reporting_service.py
+++ server/services/error_reporting_service.py
@@ -25,27 +25,3 @@ def create_error(project_id, organization_id):
     except Exception as e:
         print_exc()
         return jsonify({"STATUS": "FAIL", "MSG": "Server Error"}), 501
-
-@app.route('/errors/all')
-@error_view_access_required
-def get_all_errors(project_id, organization_id):
-    try:
-        data = bug_service.get_all_bugs(project_id, organization_id)
-        return jsonify({"STATUS": "OK", "DATA": data})
-    except BuglyticsException as e:
-        return jsonify({"STATUS": "FAIL", "MSG": str(e)})
-    except Exception as e:
-        print_exc()
-        return jsonify({"STATUS": "FAIL", "MSG": "Server Error"}), 501
-
-@app.route('/errors')
-@error_view_access_required
-def get_error(_, organization_id):
-    try:
-        bug_id = request.args['bugid']
-        data = bug_service.get_specific_bug(bug_id, organization_id)
-    except BuglyticsException as e:
-        return jsonify({"STATUS": "FAIL", "MSG": str(e)})
-    except Exception as e:
-        print_exc()
-        return jsonify({"STATUS": "FAIL", "MSG": "Server Error"}), 501
