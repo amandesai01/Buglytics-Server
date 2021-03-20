cb5679ea9caffd4ee8f8c920b92c9189203c460f
Aman Desai <amandesai01@gmail.com>
2021-03-20 12:03:44 +0530

    fix: query update

--- server/datastore/projects.py
+++ server/datastore/projects.py
@@ -2,13 +2,17 @@ from server.properties import get_pg_connection
 
 QUERY_INSERT_PROJECT = "INSERT INTO projects(project_id, organization_id, title, details, created_ts) VALUES (%s, %s, %s, %s, %s)"
 
-QUERY_GET_ALL_PROJECTS = "SELECT (project_id, title, details, created_ts, bug_count) FROM projects WHERE organization_id = %s INNER JOIN \
-                        (SELECT project_id, COUNT(bug_id) FROM bugs GROUP BY project_id) \
-                        ON projects.project_id = bugs.project_id"
+QUERY_GET_ALL_PROJECTS = "SELECT * FROM \
+        (SELECT project_id, title, details, created_ts FROM projects WHERE organization_id = %s) t1 \
+        INNER JOIN \
+        (SELECT project_id, COUNT(bug_id) as bug_count FROM bugs AS bugs GROUP BY project_id) t2 \
+        ON t1.project_id = t2.project_id"
 
-QUERY_GET_SPECIFIC_PROJECT = "SELECT (project_id, title, details, created_ts, bug_count) FROM projects WHERE project_id = %s AND organization_id = %s INNER JOIN \
-                        (SELECT project_id, COUNT(bug_id) FROM bugs) \
-                        ON projects.project_id = bugs.project_id"
+QUERY_GET_SPECIFIC_PROJECT = "SELECT * FROM \
+        (SELECT project_id, title, details, created_ts FROM projects WHERE project_id = %s AND organization_id = %s) t1 \
+        INNER JOIN \
+        (SELECT project_id, COUNT(bug_id) as bug_count FROM bugs AS bugs GROUP BY project_id) t2 \
+        ON t1.project_id = t2.project_id"
 
 db = get_pg_connection()
 
