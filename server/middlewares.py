42166511856c91ad7419acf604183e54a9b7c9ae
Aman Desai <amandesai01@gmail.com>
2021-03-20 12:03:08 +0530

    middlewares: exception printing

--- server/middlewares.py
+++ server/middlewares.py
@@ -4,6 +4,8 @@ from functools import wraps
 from server import utils
 from server.properties import get_site_secret_key
 
+from traceback import print_exc
+
 def get_decoded_token_from_header(request_header):
     if 'token' in request_header:
         obj = utils.get_object_from_token(request_header['token'], get_site_secret_key())
@@ -30,6 +32,7 @@ def error_view_access_required(f):
                 return f(obj['PROJ_ID'], obj['ORG_ID'])
             return jsonify({ "STATUS" : "FAIL", "MSG": "Contact Organization Owner to Provide Access." }), 401
         except:
+            print_exc()
             return jsonify({"STATUS": "FAIL", "MSG": "ERROR IN AUTHENTICATION"}), 401
     return decorated_function
 
@@ -42,5 +45,6 @@ def organization_required(f):
                 return f(obj['ORG_ID'])
             return jsonify({ "STATUS" : "FAIL", "MSG": "Contact Organization Owner to Provide Access." }), 401
         except:
+            print_exc()
             return jsonify({"STATUS": "FAIL", "MSG": "ERROR IN AUTHENTICATION"}), 401
     return decorated_function
