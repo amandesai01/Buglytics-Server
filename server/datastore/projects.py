074663652aa3d5d4aa627bb7af033efb565e0514
Aman Desai <amandesai01@gmail.com>
2021-03-24 14:10:35 +0530

    not feching bug count

--- server/datastore/projects.py
+++ server/datastore/projects.py
@@ -2,17 +2,9 @@ from server.properties import get_pg_connection
 
 QUERY_INSERT_PROJECT = "INSERT INTO projects(project_id, organization_id, title, details, created_ts) VALUES (%s, %s, %s, %s, %s)"
 
-QUERY_GET_ALL_PROJECTS = "SELECT * FROM \
-        (SELECT project_id, title, details, created_ts FROM projects WHERE organization_id = %s) t1 \
-        INNER JOIN \
-        (SELECT project_id, COUNT(bug_id) as bug_count FROM bugs AS bugs GROUP BY project_id) t2 \
-        ON t1.project_id = t2.project_id"
+QUERY_GET_ALL_PROJECTS = "SELECT project_id, title, details, created_ts FROM projects WHERE organization_id = %s ORDER BY created_ts DESC"
 
-QUERY_GET_SPECIFIC_PROJECT = "SELECT * FROM \
-        (SELECT project_id, title, details, created_ts FROM projects WHERE project_id = %s AND organization_id = %s) t1 \
-        INNER JOIN \
-        (SELECT project_id, COUNT(bug_id) as bug_count FROM bugs AS bugs GROUP BY project_id) t2 \
-        ON t1.project_id = t2.project_id"
+QUERY_GET_SPECIFIC_PROJECT = "SELECT project_id, title, details, created_ts FROM projects WHERE project_id = %s AND organization_id = %s"
 
 db = get_pg_connection()
 
